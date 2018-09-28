8e506d5825d9a4845df4e22eabf6b67623cdfd44
lishan <lishan>
2018-09-28 22:23:03 +0800

    9.28

--- pages/mall/newest/newest.js
+++ pages/mall/newest/newest.js
@@ -6,10 +6,41 @@ Page({
    * 页面的初始数据
    */
   data: {
+    autoplay: true,
+    dotsBoll: true,
+    interval: 2000,
+    duration: 1000,
+    current: 0,
     movies: [],
     activities:[],
     mallChosenGoods:[],
     baseUrl: app.globalData.imageUrl,
+  },
+  intervalChange: function (e) {//自动切换时间间隔
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {//滑动动画时长
+    duration: e.detail.value
+  },
+  changeIndicatorDots: function (e) {//是否显示小圆圈
+    this.setData({
+      dotsBoll: !this.data.dotsBoll
+    })
+  },
+  changeAutoplay: function (e) {//是否自动播放
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  swipclick: function (e) {//点击图片触发事件
+    console.log(this.data.imageUrls[this.data.current]);
+  },
+  bindchange: function (e) {//轮播图发生改变
+    this.setData({
+      current: e.detail.current
+    })
   },
   getUrl: function () {
     wx.navigateToMiniProgram({
@@ -47,6 +78,7 @@ Page({
             }
           }
         }
+        console.log(arrMall)
         _this.setData({
           movies: obj.banners,
           activities: obj.activities,
