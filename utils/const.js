a4366e9aee203bd871c7268f726e7da30dee6db6
xiaoni.li <xiaoli.li@youlife.me>
2018-12-07 19:08:29 +0800

    12.7

--- utils/const.js
+++ utils/const.js
@@ -1,14 +1,14 @@
-export const baseUrl = 'https://dev-mall.youlife.me'
-export const imageUrl = 'https://dev-image.youlife.me/'
-export const uploadImg = 'https://dev-mall.youlife.me/base/image'
-export const payUrl = "https://dev-pay.youlife.me/api/pay"
-export const cloudUrl = "https://dev-www.youlife.me"
+// export const baseUrl = 'https://dev-mall.youlife.me'
+// export const imageUrl = 'https://dev-image.youlife.me/'
+// export const uploadImg = 'https://dev-mall.youlife.me/base/image'
+// export const payUrl = "https://dev-pay.youlife.me/api/pay"
+// export const cloudUrl = "https://dev-www.youlife.me"
 
 
-// export const cloudUrl = "https://www.youlife.me"
-// export const payUrl = "https://pay.youlife.me/api/pay"
-// export const baseUrl = 'https://mall.youlife.me'
-// export const imageUrl = 'https://image.youlife.me/'
-// export const uploadImg = 'https://mall.youlife.me/base/image'
+export const cloudUrl = "https://www.youlife.me"
+export const payUrl = "https://pay.youlife.me/api/pay"
+export const baseUrl = 'https://mall.youlife.me'
+export const imageUrl = 'https://image.youlife.me/'
+export const uploadImg = 'https://mall.youlife.me/base/image'
 export const basicAuthorization= 'Basic QmVpSmluZ0JhaVJvbmdTaGlNYW9DbGllbnQ6ZTU2YThmMWZkOWJlMmMzMzNmYjdiZTcyNjVkMjRhYTM=';
 export const mallCode= '1000'
